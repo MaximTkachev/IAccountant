b85a870bf5512621a7ad03b7efd0798d3b9e0e85
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-24 22:11:32 +0700

    Добавлен метод получения операции по id

--- src/iaccountant/api/operations.py
+++ src/iaccountant/api/operations.py
@@ -7,15 +7,19 @@ from iaccountant.services.operations import OperationsService
 from iaccountant.models.operations import Operation
 from iaccountant.models.auth import User
 from iaccountant.models.operations import OperationCreate
+from iaccountant.api.files import router as files_router
+
+from iaccountant.tables import Operation as op
 
 router = APIRouter(
     prefix='/operations',
     tags=['Operations']
 )
+router.include_router(files_router)
 
 
 @router.get('/', response_model=List[Operation])
-def get_all_operations(
+def get_all_operations_of_current_user(
         kind: Optional[OperationKind] = None,
         user: User = Depends(get_current_user),
         service: OperationsService = Depends()
@@ -34,3 +38,12 @@ def create_operation(
     return service.create(
         user_id=user.id, operation_data=operation_data
     )
+
+
+@router.post('/{operation_id}', response_model=Operation)
+def get_operation_by_id(
+        operation_id: int,
+        user: User = Depends(get_current_user),
+        service: OperationsService = Depends()
+):
+    return service.get_operation_by_id(operation_id=operation_id, author_id=user.id)
